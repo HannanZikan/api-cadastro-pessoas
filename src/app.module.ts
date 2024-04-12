b590b0febb28020fbaf57f0301bcd597e29cff19
HannanZikan <hannanczikan@gmail.com>
2024-04-12 17:21:00 -0300

    adde typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,26 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PeopleModule } from './people/people.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [PeopleModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot(),
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: process.env.MYSQL_HOST,
+            port: Number(process.env.MYSQL_LOCAL_PORT),
+            username: process.env.MYSQL_USERNAME,
+            password: process.env.MYSQL_PASSWORD,
+            database: process.env.MYSQL_DATABASE,
+            synchronize: false,
+        }),
+        PeopleModule,
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }

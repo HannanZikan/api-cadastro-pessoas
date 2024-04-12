9394c02860be56de46f0786608c87b4f49e80168
HannanZikan <hannanczikan@gmail.com>
2024-04-12 17:52:34 -0300

    fix: add user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,21 +2,24 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { UserController } from './user/user.controller';
 
 @Module({
-    imports: [
-        ConfigModule.forRoot(),
-        TypeOrmModule.forRoot({
-            type: 'mysql',
-            host: process.env.MYSQL_HOST,
-            port: Number(process.env.MYSQL_LOCAL_PORT),
-            username: process.env.MYSQL_USERNAME,
-            password: process.env.MYSQL_PASSWORD,
-            database: process.env.MYSQL_DATABASE,
-            synchronize: false,
-        }),
-    ],
-    controllers: [],
-    providers: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQL_HOST,
+      port: Number(process.env.MYSQL_LOCAL_PORT),
+      username: process.env.MYSQL_USERNAME,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+      synchronize: false,
+    }),
+    UserModule,
+  ],
+  controllers: [UserController],
+  providers: [],
 })
-export class AppModule { }
+export class AppModule {}
